70074c4fc4086aa8e9ccb6fa0cb4c0306893d04e
Qadriga <rob.sandhausen@freenet.de>
2017-03-19 13:37:11 +0100

    make the prototyes form c++ to c

--- session.c
+++ session.c
@@ -1,6 +1,2 @@
 #include "session.h"
 
-Session::Session()
-{
-
-}
--- session.h
+++ session.h
@@ -1,11 +1,16 @@
 #ifndef SESSION_H
 #define SESSION_H
+#include <glib.h>
 
+struct _Session{
+    GDate* Expiredate;
 
-class Session
-{
-public:
-    Session();
+    GList* user_data;
 };
 
+
+typedef struct _Session Session;
+Session* session_new();
+gboolean session_set_expiredate(GDate* Expiredate);
+gboolean session_is_expired(Session* session);
 #endif // SESSION_H
