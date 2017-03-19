65d8607cb4251e8ca57c95e34787953d5fc1b941
Qadriga <rob.sandhausen@freenet.de>
2017-03-19 13:34:26 +0100

    added structs for request and response note parse from/to string are not implemented

--- data_dispatch.c
+++ data_dispatch.c
@@ -1,6 +1,80 @@
 #include "data_dispatch.h"
 
-data_dispatch::data_dispatch()
-{
+void data_dispatch_init(data_dispatch **data){
+    (*data)->connection = NULL;
+}
+
+data_dispatch* data_dispatch_new(){
+    data_dispatch* data;
+    data = (data_dispatch*) g_try_malloc(sizeof(data_dispatch));
+    data_dispatch_init(&data);
+    return data;
+}
+
+void http_request_init(http_request **request){
+    (*request)->accepts = NULL;
+    (*request)->body = g_array_new(FALSE,TRUE,sizeof(gchar));
+    (*request)->path = NULL;
+    (*request)->user_agent = NULL;
+    (*request)->protokoll_version = 1.0;
+
+}
+http_request* http_request_new(){
+    http_request* data;
+    data = (http_request*) g_try_malloc0(sizeof(http_request));
+    http_request_init(&data);
+    return data;
+}
+
+void http_response_init(http_response **response){
+    (*response)->content_lenght = 0;
+    (*response)->content_type = NULL;
+    (*response)->date = g_date_new();
+    (*response)->http_status_code = 200;
+    (*response)->protokoll_verion = 1.0;
+    (*response)->srver_name = NULL;
+}
+
+http_response* http_response_new(){
+    http_response* data;
+    data = (http_response*) g_try_malloc0(sizeof(http_response));
+    http_response_init(&data);
+    return data;
+}
+
+gboolean parse_http_request(const gchar *raw_request_string, http_request *request, GError **erroer){
+
+}
+gboolean parse_http_response(http_response *response, gchar **response_string, GError **error){
+
+}
+
+gboolean clear_http_request(http_request *request){
+    g_array_unref(request->body);
+    g_list_free(request->accepts);
+    g_list_free(request->path);
+    g_list_free(request->user_agent);
+    return TRUE;
+
+}
+
+gboolean clear_http_response(http_response *response){
+    g_array_unref(response->body);
+    g_date_free(response->date);
+    g_free(response->srver_name);
+    g_list_free(response->content_type);
+    return TRUE;
+}
+
+gboolean reset_http_request(http_request *request){
+    clear_http_request(request);
+    http_request_init(&request);
+    return TRUE;
+
+}
+gboolean reset_http_response(http_response *response){
+    clear_http_response(response);
+    http_response_init(&response);
+    return TRUE;
 
 }
--- data_dispatch.h
+++ data_dispatch.h
@@ -1,11 +1,63 @@
 #ifndef DATA_DISPATCH_H
 #define DATA_DISPATCH_H
+#include <glib.h>
+#include <gio/gio.h>
 
+struct _data_dispatch{
+    GSocketConnection* connection;
 
-class data_dispatch
-{
-public:
-    data_dispatch();
 };
+
+enum _http_type{
+    GET,POST,HEAD,PUT,OPTIONS,DELETE,TRACE,CONNECT
+};
+enum _http_connection_type{
+    keep_alive,close
+};
+
+struct _http_request{
+    GList* path;
+    http_type type;
+    gfloat protokoll_version;
+    GList* accepts;
+    GList* user_agent;
+    gchar* host;
+    http_connection_type connection_type;
+
+    GArray* body;
+
+};
+
+struct _http_response{
+    gfloat protokoll_verion;
+    guint http_status_code;
+    GDate* date;
+    gchar* srver_name;
+    guint content_lenght;
+    GList* content_type;
+
+    GArray* body;
+};
+
+typedef struct _data_dispatch data_dispatch;
+typedef struct _http_request http_request;
+typedef struct _http_response http_response;
+typedef enum _http_connection_type http_connection_type;
+typedef enum _http_request http_type;
+
+void data_dispatch_init(data_dispatch** data);
+data_dispatch* data_dispatch_new(void);
+
+void http_request_init(http_request** request);
+http_request* http_request_new(void);
+gboolean clear_http_request(http_request* request);
+gboolean reset_http_request(http_request* request);
+gboolean parse_http_request(const gchar *raw_request_string, http_request *request, GError **erroer);
+
+void http_response_init(http_response** response);
+http_response* http_response_new(void);
+gboolean clear_http_response(http_response* response);
+gboolean reset_http_response(http_response* response);
+gboolean parse_http_response(http_response* response, gchar** response_string, GError** error);
 
 #endif // DATA_DISPATCH_H
